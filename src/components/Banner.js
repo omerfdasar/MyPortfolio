c06a84d9567b534e6f9976db13ade8c7651a06eb
Omer <omerfarukdasar@gmail.com>
2022-07-18 21:58:26 +0200

    Banner Finished

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,6 +5,54 @@ import { ArrowRightCircle } from "react-bootstrap-icons";
 // import "animate.css";
 // import TrackVisibility from "react-on-screen";
 const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [
+    "Frontend Developer",
+    "Web Developer",
+    "Full Stack Developer",
+  ];
+  const period = 2000;
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex((prevIndex) => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
   return (
     <section className="banner" id="home">
       <Container>
@@ -12,7 +60,7 @@ const Banner = () => {
           <Col xs={12} md={6} xl={7}>
             <span className="tagline">Welcome to my Portfolio</span>
             <h1>
-              {`Hi I'm Omer`} <span className="wrap">web developer</span>
+              {`Hi I'm Omer`} <span className="wrap">{text}</span>
             </h1>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ipsa
