3c7e1fb82e51c8e1d85b0301fdc10a7a2d5537f1
Omer <omerfarukdasar@gmail.com>
2022-07-19 15:40:56 +0200

    template done

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,8 +2,8 @@ import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
 import { ArrowRightCircle } from "react-bootstrap-icons";
-// import "animate.css";
-// import TrackVisibility from "react-on-screen";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
@@ -55,32 +55,30 @@ const Banner = () => {
 
   return (
     <section className="banner" id="home">
-      <Container>
-        <Row className="align-items-center">
-          <Col xs={12} md={6} xl={7}>
-            <span className="tagline">Welcome to my Portfolio</span>
-            <h1>
-              {`Hi I'm Omer`} <br />
-              <span className="wrap">{text}</span>
-            </h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ipsa
-              quam iusto, vero nobis sequi!
-            </p>
-            <button
-              onClick={() => {
-                console.log("connect");
-              }}
-            >
-              Let's Connect <ArrowRightCircle size={25} />
-            </button>
-          </Col>
-          <Col xs={12} md={6} xl={5}>
-            <img src={headerImg} alt="header-img" />
-          </Col>
-        </Row>
-      </Container>
-    </section>
+    <Container>
+      <Row className="aligh-items-center">
+        <Col xs={12} md={6} xl={7}>
+          <TrackVisibility>
+            {({ isVisible }) =>
+            <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+              <span className="tagline">Welcome to my Portfolio</span>
+              <h1>{`Hi! I'm Omer`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+            </div>}
+          </TrackVisibility>
+        </Col>
+        <Col xs={12} md={6} xl={5}>
+          <TrackVisibility>
+            {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                <img src={headerImg} alt="Header Img"/>
+              </div>}
+          </TrackVisibility>
+        </Col>
+      </Row>
+    </Container>
+  </section>
   );
 };
 
